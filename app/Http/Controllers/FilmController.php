860dd632299c3713cffd26b39d11b70ef752dadf
Alfrina29 <alfrina.g.p@gmail.com>
2022-12-14 10:49:26 +0700

    modified:   app/Http/Controllers/FilmController.php modified:   resources/views/film.blade.php

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -4,15 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Film;
+use DB;
 
 class FilmController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         
         return view('film', [
             "tittle" => "Daftar Film",
-            "lists" =>  Film::latest()->filter(request(['search', 'genre']))->get()
+            "lists" =>  DB::table('films')->join('genres','films.genre_id','=','genres.id')->where('genres.genre','like','%'.$request->search.'%')->orWhere('films.tittle','like','%'.$request->search.'%')->get()
         ]);
     }
 }
--- resources/views/film.blade.php
+++ resources/views/film.blade.php
@@ -27,10 +27,7 @@
         <br></br>
         <div class="row justify-content-center mb-3">
             <div class="col-md-6">
-                <form action="/film">
-                  @if (request('genre'))
-                  <input type="hidden" name="genre" value="{{ request('genre') }}">   
-                  @endif
+                <form action="/film" method="get">
                   <div class="input-group mb-3">
                     <input type="text" class="form-control" placeholder="Search.."
                         name="search" value="{{ request('search') }}">
@@ -48,7 +45,7 @@
                             <img src="..." class="card-img-top" alt="...">
                             <h3 class="card-tittel">{{ $list->tittle }}</h3>
                             <p class="card-text">Tahun Rilis : {{ $list->tahun }}</p>
-                            <p>Genre Film : {{ $list->genre->genre }}</p>
+                            <p>Genre Film : {{ $list->genre}}</p>
                         </div>
                     </div>
                 @endforeach
